1552614cd8b61f0d4d890eb051a9baafc3644789
Rohit <annamaneni12rohit@gmail.com>
2025-11-16 14:43:54 +0530

    Downstream testing

---
+++ src/downstream.py
@@ -0,0 +1,171 @@
+from ultralytics import YOLO
+import torch
+import cv2
+import os
+from PIL import Image
+import numpy as np
+import sys
+# from CNN_based.inference import load_model
+# from CNN_based.train import SRCNN
+import torch.nn as nn
+from iterative_backprojection import IBP
+
+class SRCNN(nn.Module):
+    def __init__(self, num_channels=1):
+        super(SRCNN, self).__init__()
+        self.conv1 = nn.Conv2d(num_channels, 64, kernel_size=9, padding=9 // 2)
+        self.conv2 = nn.Conv2d(64, 32, kernel_size=5, padding=5 // 2)
+        self.conv3 = nn.Conv2d(32, num_channels, kernel_size=5, padding=5 // 2)
+        self.relu = nn.ReLU(inplace=True)
+
+    def forward(self, x):
+        x = self.relu(self.conv1(x))
+        x = self.relu(self.conv2(x))
+        x = self.conv3(x)
+        return x
+
+def run_yolo(source, file_name, task):
+
+    # GPU support
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    print("Using device:", device)
+
+    model = YOLO("yolov8n.pt")
+    model.to(device)
+
+    # Run inference
+    results = model(source, device=device)
+
+    # Vehicle class IDs in COCO
+    vehicle_ids = {2, 3, 4, 5, 6, 7, 8}
+
+    output_string = ""
+
+    avg_confidence = 0.0
+    total_vehicles = 0
+
+    for result in results:
+        final_boxes = []
+        boxes = result.boxes
+
+
+        for box in boxes:
+            cls = int(box.cls[0])
+
+            # Only keep vehicles
+            if cls not in vehicle_ids:
+                continue
+            else:
+                final_boxes.append(box)
+
+            x1, y1, x2, y2 = box.xyxy[0].tolist()
+            conf = float(box.conf[0])
+            
+            avg_confidence += conf
+            total_vehicles += 1
+
+            cls_name = model.names[cls]
+
+            output_string += (
+                f"Class: {cls_name}, "
+                f"Conf: {conf:.3f}, "
+                f"Box: ({x1:.1f}, {y1:.1f}, {x2:.1f}, {y2:.1f})\n"
+            )
+
+        result.boxes = final_boxes
+        avg_confidence = (avg_confidence / total_vehicles) if total_vehicles > 0 else 0.0
+
+    print("Vehicle Detections:\n", output_string if output_string else "No vehicles detected.")
+
+    # Save annotated output image
+    result_img = results[0].plot()
+    annotated_dir = f"annotated_outputs/{task}"
+    os.makedirs(annotated_dir, exist_ok=True)
+    out_path = f"{annotated_dir}/{file_name}"
+    cv2.imwrite(out_path, result_img)
+    # print("Annotated image saved to:", out_path)
+
+    return avg_confidence, out_path
+
+
+if __name__ == "__main__":
+    # car_images_dir = "D:\\DIP Project\\car_testing"
+    # factor = 3
+    # tasks = ["Bicubic", "SRCNN", "IBP"]
+
+    # model = SRCNN().to(torch.device("cuda" if torch.cuda.is_available() else "cpu"))
+    # model.load_state_dict(torch.load("D:\\DIP Project\\srcnn_x3.pth", map_location=torch.device("cuda" if torch.cuda.is_available() else "cpu")))
+    # model.eval()
+
+    # for task in tasks:
+    #     task_dir = os.path.join(car_images_dir, task)
+    #     os.makedirs(task_dir, exist_ok=True)
+
+    # for img_name in os.listdir(car_images_dir):
+    #     if not img_name.lower().endswith(('.png', '.jpg', '.jpeg', '.bmp', '.tiff')):
+    #         continue
+    #     print(f"Processing image: {img_name}")
+    #     for task in tasks:
+    #         print(f"  Applying task: {task}")
+    #         img_path = os.path.join(car_images_dir, img_name)
+    #         img = np.array(Image.open(img_path).convert("L"))
+            
+    #         task_img_path = os.path.join(car_images_dir, task, img_name)
+    #         if task == "Bicubic":
+    #             img_pil = Image.fromarray(img)
+    #             new_size = (img_pil.width//factor, img_pil.height//factor)
+    #             img_resized = img_pil.resize(new_size, Image.BICUBIC)
+    #             new_size = (img_resized.width*factor, img_resized.height*factor)
+    #             img_resized = img_resized.resize(new_size, Image.BICUBIC)
+    #             img_resized.save(task_img_path)
+    #         elif task == "SRCNN":
+    #             img_pil = Image.fromarray(img)
+    #             new_size = (img_pil.width//factor, img_pil.height//factor)
+    #             img_resized = img_pil.resize(new_size, Image.BICUBIC)
+    #             new_size = (img_resized.width*factor, img_resized.height*factor)
+    #             img_resized = img_resized.resize(new_size, Image.BICUBIC)
+
+    #             img_resized = np.array(img_resized).astype(np.float32) / 255.0
+    #             input_tensor = torch.from_numpy(img_resized).unsqueeze(0).unsqueeze(0).to(torch.device("cuda" if torch.cuda.is_available() else "cpu"))
+    #             with torch.no_grad():
+    #                 output_tensor = model(input_tensor)
+    #             output_img = output_tensor.squeeze().cpu().numpy()
+    #             output_img = (output_img * 255.0).clip(0, 255).astype(np.uint8)
+    #             Image.fromarray(output_img).save(task_img_path)
+    #         elif task == "IBP":
+
+    #             img_pil = Image.fromarray(img)
+    #             new_size = (img_pil.width//factor, img_pil.height//factor)
+    #             img_resized = img_pil.resize(new_size, Image.BICUBIC)
+    #             img_resized = np.array(img_resized).astype(np.float32)
+    #             ibp_img = IBP(img_resized, factor, factor)
+    #             ibp_img = np.clip(ibp_img, 0, 255).astype(np.uint8)
+    #             Image.fromarray(ibp_img).save(task_img_path)
+
+    tasks = ["Bicubic", "SRCNN", "IBP"]
+
+    for task in tasks:
+        img_dir = f"D:\\DIP Project\\car_testing\\{task}"
+        os.makedirs(f"annotated_outputs/{task}", exist_ok=True)
+        output_dir = f"annotated_outputs/{task}"
+
+        average_confidences = []
+        for img_name in os.listdir(img_dir):
+            if not img_name.lower().endswith(('.png', '.jpg', '.jpeg', '.bmp', '.tiff')):
+                continue
+            print(f"Running YOLO on {task} image: {img_name}")
+            
+            img_path = os.path.join(img_dir, img_name)
+            avg_conf, out_path = run_yolo(img_path, img_name, task)
+
+            average_confidences.append(avg_conf)
+
+            print(f"Average confidence for {img_name} ({task}): {avg_conf:.3f}\n")
+
+        overall_avg_conf = (sum(average_confidences) / len(average_confidences)) if average_confidences else 0.0
+        print(f"Overall average confidence for {task}: {overall_avg_conf:.3f}\n")
+
+        txt_file = open(f"{output_dir}/average_confidence.txt", "w")
+        txt_file.write(f"Overall average confidence for {task}: {overall_avg_conf:.3f}\n")
+        txt_file.close()
+
--- test_ibp.py
+++ test_ibp.py
@@ -4,7 +4,31 @@ import numpy as np
 import cv2
 import os
 
-output_dir = "D:\\DIP Project\\outputs\\IBP"
+# output_dir = "D:\\DIP Project\\outputs\\IBP"
+# os.makedirs(output_dir, exist_ok=True)
+# prefix = "D:\\DIP Project\\grayscale_LR_images"
+# subdirs = ["BSD100", "Set5", "Set14"]
+# suffix = "image_SRF_3"
+
+# for subdir in subdirs:
+#     test_dir = os.path.join(prefix, subdir)
+#     image_paths = sorted(os.listdir(test_dir))
+
+#     os.makedirs(os.path.join(output_dir, subdir), exist_ok=True)
+
+#     for image_path in image_paths:
+#         print(f"IBP {image_path}...")
+
+#         lr_image = Image.open(os.path.join(test_dir, image_path)).convert("L")
+#         # lr_image = np.array(lr_image.resize((lr_image.width * 3, lr_image.height * 3), Image.BICUBIC)).astype(np.float32)/255.0
+        
+#         lr_image = np.array(lr_image).astype(np.float32)
+
+#         sr_image = IBP(lr_image, factor_x=3, factor_y=3)
+
+#         cv2.imwrite(os.path.join(output_dir, subdir, image_path), sr_image)
+
+output_dir = "D:\\DIP Project\\outputs\\Bicubic"
 os.makedirs(output_dir, exist_ok=True)
 prefix = "D:\\DIP Project\\grayscale_LR_images"
 subdirs = ["BSD100", "Set5", "Set14"]
@@ -17,13 +41,14 @@ for subdir in subdirs:
     os.makedirs(os.path.join(output_dir, subdir), exist_ok=True)
 
     for image_path in image_paths:
-        print(f"IBP {image_path}...")
+        print(f"Bicubic {image_path}...")
 
         lr_image = Image.open(os.path.join(test_dir, image_path)).convert("L")
         # lr_image = np.array(lr_image.resize((lr_image.width * 3, lr_image.height * 3), Image.BICUBIC)).astype(np.float32)/255.0
         
         lr_image = np.array(lr_image).astype(np.float32)
 
-        sr_image = IBP(lr_image, factor_x=3, factor_y=3)
+        # sr_image = IBP(lr_image, factor_x=3, factor_y=3)
+        sr_image = cv2.resize(lr_image, (lr_image.shape[1]*3, lr_image.shape[0]*3), interpolation=cv2.INTER_CUBIC)
 
         cv2.imwrite(os.path.join(output_dir, subdir, image_path), sr_image)
